89850a08a2f8ca9eb79d7a0a3494052b89b2c188
Chamane Barbatti <chamane@sbcglobal.net>
2022-11-16 16:10:14 -0600

    help

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -2,25 +2,82 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+
+
+
+
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace coding_events_practice.Controllers
+namespace CodingEventsDemo.Controllers
+
 {
     public class EventsController : Controller
     {
+        private EventDbContext context;
+        public EventsController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<string> Events = new List<string>();
-            Events.Add("Code With Pride");
-            Events.Add("Apple WWDC");
-            Events.Add("Strange Loop");
+            List<Event> events = context.Events.ToList();
+            
 
-            ViewBag.events = Events;
+            return View(events);
+        }
+        public IActionResult Add()
+        {
+            
+                AddEventViewModel addEventViewModel = new AddEventViewModel();
+            
+            return View(addEventViewModel);
+        }
+        [HttpPost]
+            public IActionResult Add(AddEventViewModel addEventViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Event newEvent = new Event
+                {
+                    Name = addEventViewModel.Name,
+                    Description = addEventViewModel.Description,
+                    ContactEmail = addEventViewModel.ContactEmail,
+                    Type = addEventViewModel.Type
 
+                };
+                context.Events.Add(newEvent);
+                context.SaveChanges();
+
+                return Redirect( "/Events");
+
+            }
+                return View(addEventViewModel);
+            
+        }
+        public IActionResult Delete()
+        {
+            ViewBag.events = context.Events.ToList();
             return View();
+
+        }
+        [HttpPost]
+        public IActionResult Delete(int[] eventsIds)
+        {
+            foreach (int eventId in eventsIds)
+            {
+               Event theEvent = context.Events.Find(eventId);
+                context.Events.Remove(theEvent);
+            }
+            context.SaveChanges();
+            return Redirect("/Events");
         }
     }
 }
---
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description too long!")]
+        public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+
+        [Required(ErrorMessage = "Location information is required.")]
+        public string Location { get; set; }
+
+        public bool IsTrue { get { return true; } }
+
+        [Compare("IsTrue", ErrorMessage = "Registration is required.")]
+        public bool RegistrationRequired { get; set; }
+
+        [Range(0, 100000, ErrorMessage = "The number of attendees must be between 0 and 100,000.")]
+        public int NumberOfAttendees { get; set; }
+        public string Type { get; set; }
+    }
+}
+
